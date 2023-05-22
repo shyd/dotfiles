be544d3fb83d1d6b70113c63ee33948cd52afcd3
Dennis <dennis@schuett.io>
2023-05-22 16:51:16 +0200

    Update git repos on dotfiles-update

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+set -e
+
+pull_or_clone () {
+    echo "$2"
+    git -C "$2" pull || git clone --depth 1 $1 "$2"
+    echo ""
+}
 
 # Install oh-my-zsh & plugins
 if [ ! -d ~/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
-git clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
-git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
-git clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
-git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+pull_or_clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+pull_or_clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+pull_or_clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+pull_or_clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+pull_or_clone https://github.com/supercrabtree/k ~/.oh-my-zsh/custom/plugins/k
+pull_or_clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
+pull_or_clone https://github.com/Aloxaf/fzf-tab ~/.oh-my-zsh/custom/plugins/fzf-tab
+pull_or_clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
 
 # Create z file
 touch ~/.z
 
 # Install asdf and plugins
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+pull_or_clone https://github.com/asdf-vm/asdf.git ~/.asdf
 . $HOME/.asdf/asdf.sh
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
@@ -41,21 +48,21 @@ do
 
     ln -s ~/.dotfiles/$dotfile ~/$dotfile
     echo "created symlink $dotfile"
-    
+
 done
 
 yes | cp -f ~/.dotfiles/.gitconfig ~/.gitconfig
 
 # Install vim themes & plugins
-git clone https://github.com/dracula/vim.git ~/.vim/pack/themes/start/dracula
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+pull_or_clone https://github.com/dracula/vim.git ~/.vim/pack/themes/start/dracula
+pull_or_clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+pull_or_clone https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 wget https://raw.githubusercontent.com/junegunn/fzf-git.sh/main/fzf-git.sh -qO ~/.fzf-git.sh
 
 # install tmux plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+pull_or_clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # start a server but don't attach to it
 tmux start-server
 # create a new session but don't attach to it either
